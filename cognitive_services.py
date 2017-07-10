097881dde6859ca1fa98fa0537b48ca2d34fc8a8
duanyanan <duanyanan96@outlook.com>
2017-07-10 10:27:44 +0800

    add speech api

--- cognitive_services.py
+++ cognitive_services.py
@@ -4,6 +4,7 @@ import urllib.request
 import urllib.parse
 import urllib.error
 from device import Camera
+import requests
 
 
 class EmotionAPI(object):
@@ -44,6 +45,7 @@ class EmotionAPI(object):
         return self.emotion
 
 
+
 class SpeechAPI(object):
     def __init__(self):
         self.token_url = 'https://api.cognitive.microsoft.com' \
@@ -53,9 +55,11 @@ class SpeechAPI(object):
         self.api_url = 'https://speech.platform.bing.com' \
                        '/speech/recognition/interactive/cognitiveservices/v1?language=zh-CN'
         self.api_headers = {'Content-type': 'audio/wav; codec="audio/pcm"; samplerate=16000',
-                            'Transfer-Encoding': 'chunked'}
+                            'Transfer-Encoding': 'chunked',
+                            'Authorization': 'Bearer ' + self.token}
         self.token = ""
-               # 'Authorization': 'Bearer ' + token
+        # 'Authorization': 'Bearer ' + token
+        self.speeech = ""
 
     def read_in_block(self, filename='temp/temp.wav'):
         block_size = 1024
@@ -68,24 +72,19 @@ class SpeechAPI(object):
                     return
 
 
-
-    myHeaders = {'Ocp-Apim-Subscription-Key': '481f85de65d94ecf9c660d3a715a1521'
-                 }
-    url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
-
-    response = requests.post(url, headers=myHeaders)
-    response.raise_for_status()
-    token = response.text
-    print(token)
+    def access_token(self):
+        myHeaders = self.token_headers
+        url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
+        response = requests.post(self.token_url, headers=self.token_headers)
+        response.raise_for_status()
+        self.token = response.text
+        print(self.token)
 
 
+    def get_speech_service(self):
+        r = requests.post(self.api_url, headers=self.api_headers, data=self.read_in_block())
+        print(r.text)
+        print(r.status_code)
+        return r.text
 
-    url = 'https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/v1?language=zh-CN'
-    headers = {'Content-type': 'audio/wav; codec="audio/pcm"; samplerate=16000',
-               'Transfer-Encoding': 'chunked',
-               'Authorization': 'Bearer ' + token
-               }
-    r = requests.post(url, headers=headers, data=read_in_block())
 
-    print(r.text)
-    print(r.status_code)
